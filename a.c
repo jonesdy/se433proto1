acbef37c3ff82ede7195ebd3e1f9f859eddd027e
Dylan Jones <jonesdyn@gmail.com>
2014-09-28 13:06:29 -0500

    Removing struct, formatting

--- a.c
+++ a.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 /*threads*/
 #include <pthread.h>
@@ -20,13 +21,11 @@
 /*timeout sets errno*/
 #include <errno.h>
 
-
 #define STARTUPTIME 5
 #define RUNTIME 20
 #define LISTENTIME 10
 
 #define NUMTOSEND 20
-
 
 #define ARG_DESTIP 1
 #define ARG_SOURCEIP 2
@@ -38,68 +37,64 @@
 
 #define LISTENTIMEOUT 3
 
-typedef struct{
-	int s;
-//	char c;
-} X;
-
 //globals
 int recurrence;
 struct sockaddr_in bindto;
 time_t starttime;
 
-void * listenandprint(void *s){
+void *listenandprint(void *s)
+{
 	int *sock = (int*)s;
-	while(1){
+	while(1)
+   {
 		if(time(NULL)-starttime > RUNTIME)
+      {
 			return NULL;
-		char data[sizeof(X)]={0};
+      }
+		char data[sizeof(int32_t)] = {0};
 		
-		int numbytes = recv(*sock, data, sizeof(X), 0);
+		int numbytes = recv(*sock, data, sizeof(int32_t), 0);
 		if(errno == EAGAIN|| errno == EWOULDBLOCK ||numbytes == 0)
 		{
 			errno = 0;/*clear error flag so we don't keep continuing*/
 			continue;
 		}
-		X *rcvd = (X *)data;
-		rcvd->s = ntohl(rcvd->s);/*unpack endianness*/
+		int32_t *rcvd = (int32_t *)data;
+		*rcvd = ntohl(*rcvd);/*unpack endianness*/
 		printf("\nI am RX and I got a ");
-		printf("%d\n",rcvd->s);
+		printf("%d\n",*rcvd);
 	}
 }
 
-void * sendstuff(void *s){
+void *sendstuff(void *s)
+{
 	sleep(STARTUPTIME);
 	int sock = *(int*)s;
-	X tosend = {0};
-	while(tosend.s < NUMTOSEND){
-		tosend.s ++;
-		tosend.s = htonl(tosend.s);
+	int32_t tosend = 0;
+	while(tosend < NUMTOSEND)
+   {
+		tosend ++;
+		tosend = htonl(tosend);
 		sendto(sock, &tosend, sizeof(tosend), 0, (const struct sockaddr *)&bindto, sizeof(bindto));
-		tosend.s = ntohl(tosend.s);
-		printf("I am TX and I am going to send a %d\n", tosend.s);
+		tosend = ntohl(tosend);
+		printf("I am TX and I am going to send a %d\n", tosend);
 		usleep(recurrence*USPERMS);
-
 	}
-	printf("a nice done message\n");
+	printf("TX is done\n");
 	sleep(LISTENTIME);
 	return NULL;
-
 }
 
-
-
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	starttime = time(NULL);
-	if(argc < ARG_DELAY+1){
+	if(argc < ARG_DELAY+1)
+   {
 		printf("usage: %s destip (listenip|all) destport listenport txrate\n", argv[0]);
 		return 0;
 	}
 	recurrence = atoi(argv[ARG_DELAY]);
 	printf("delay is %d\n", recurrence);
-
-
 
 	int sock = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sock < 0){printf("error initializing socket %d", sock);return 1;}
@@ -112,11 +107,17 @@ int main(int argc, char *argv[]){
 		bindto.sin_addr.s_addr = htonl(INADDR_ANY);
 	}
 	else
+   {
 		bindto.sin_addr.s_addr = inet_addr(argv[ARG_SOURCEIP]);
+   }
 	if(bind(sock, (const struct sockaddr *)&bindto, sizeof(bindto)) == 0)
+   {
 		printf("listening on %d\n", atoi(argv[ARG_SOURCEPORT]));
+   }
 	else
+   {
 		printf("failed to bind %d (is it in use?)\n", atoi(argv[ARG_SOURCEPORT]));
+   }
 	/*socket is bound so we can now change these to the destination address and socket*/
 	bindto.sin_addr.s_addr = inet_addr(argv[ARG_DESTIP]);
 	bindto.sin_port = htons(atoi(argv[ARG_DESTPORT]));
@@ -136,5 +137,3 @@ int main(int argc, char *argv[]){
 	printf("closing socket\n");
 	close(sock);
 }
-
-
